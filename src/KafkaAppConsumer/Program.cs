de07f5eb49d0115fe9af0cee509653f2ea454ab7
Ana Oliveira <theanaoliveira@outlook.com>
2022-02-09 22:07:26 -0300

    included project

---
+++ src/KafkaAppConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KafkaAppConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
